4f7131423b1ca0f82465d7300d870a3977df70fa
Nikhila Ravi <nikkiravi92@gmail.com>
2016-04-30 18:22:15 +0100

    Adds code for ui

---
+++ scripts/s3-update-graphiql.sh
@@ -0,0 +1,13 @@
+aws s3api put-object \
+  --acl "public-read" \
+  --bucket "gql-v1" \
+  --key "graphiql/index.html" \
+  --body index.html \
+  --content-type "text/html"
+
+aws s3api put-object \
+  --acl "public-read" \
+  --bucket "gql-v1" \
+  --key "graphiql/index.html" \
+  --body index.html \
+  --content-type "text/html"
